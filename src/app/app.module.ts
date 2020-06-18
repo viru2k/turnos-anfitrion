3d4f2f8cab3389387cd1cc788d4ff819f50f39d4
viru2k <gastonvillafane@gmail.com>
2020-06-18 12:55:28 -0300

    turnos anfitrion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { LlamadorSectorComponent } from './pages/llamador/llamador-sector/llamador-sector.component';
+import { LlamadorColaComponent } from './pages/llamador/llamador-cola/llamador-cola.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LlamadorSectorComponent,
+    LlamadorColaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/llamador/llamador-sector/llamador-sector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-llamador-sector',
+  templateUrl: './llamador-sector.component.html',
+  styleUrls: ['./llamador-sector.component.scss']
+})
+export class LlamadorSectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
